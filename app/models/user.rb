a222633441a38c674e160b4888b44a755b1b869c
hamakachu <mentaiko0202@yahoo.co.jp>
2023-05-12 14:17:32 +0900

    新規登録、ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, unique: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_kana, presence: true
-  validates :first_kana, presence: true
+  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :family_kana, presence: true, format: {with: /\A[ァ-ヴー]+\z/}
+  validates :first_kana, presence: true, format: {with: /\A[ァ-ヴー]+\z/}
   validates :birth, presence: true
 end
