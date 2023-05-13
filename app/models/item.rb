bbd952165e5328d41b8c05057790db2ca2e3dbb9
hamakachu <mentaiko0202@yahoo.co.jp>
2023-05-13 18:27:38 +0900

    ChargeBearerモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,8 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :condition
-  belongs_to :
+  belongs_to :charge_bearer
+  
 
   validates :item_name, :introduce, presence: true
   validates :category_id, :condition_id, :charge_bearer_id, :prefecture_id, :shipping_date_id, numericality: 
