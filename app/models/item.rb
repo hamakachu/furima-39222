0716779ffd0df253496299388edb1efc7e91ec19
hamakachu <mentaiko0202@yahoo.co.jp>
2023-05-13 17:42:31 +0900

    categoryモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+
+  validates :item_name, :introduce, presence: true
+  validates :category_id, :condition_id, :charge_bearer_id, :prefecture_id, :shipping_date_id, numericality: 
+            { other_than: 1, message: "can't be blank" }
   
 end
