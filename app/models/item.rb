773e8a9c31cc6afed1aa3338f5ee4aca34b70973
hamakachu <mentaiko0202@yahoo.co.jp>
2023-05-13 18:00:54 +0900

    conditionモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :category
+  belongs_to :condition
+  belongs_to :
 
   validates :item_name, :introduce, presence: true
   validates :category_id, :condition_id, :charge_bearer_id, :prefecture_id, :shipping_date_id, numericality: 
             { other_than: 1, message: "can't be blank" }
-  
+
 end
