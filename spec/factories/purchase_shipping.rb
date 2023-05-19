0f26f4855ca64957ef35ffa89bb9c97e60858994
hamakachu <mentaiko0202@yahoo.co.jp>
2023-05-19 19:28:37 +0900

    ダミー値生成

--- spec/factories/purchase_shipping.rb
+++ spec/factories/purchase_shipping.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :purchase_shipping do
-    
+    association :user
+    association :item
+    postal_code   {"111" + "-" + Faker::number.number(digits: 4)}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    city          {Gimei.city.kanji}
+    address       {Gimei.town.kanji}
+    building      {Faker::Lorem.word}
+    phone         {"0" + Faker::Number.between(from: 100000000, to: 9999999999)}
+    token         {"tok_" + Faker::Faker::Alphanumeric.alphanumeric(number: 28)}
   end
 end
