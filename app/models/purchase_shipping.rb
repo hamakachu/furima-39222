f3ebcaa8f997f7535a4de22eaa84b9fdf2797632
hamakachu <mentaiko0202@yahoo.co.jp>
2023-05-20 12:09:16 +0900

    PurchaseShippingモデル単体テスト実装

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -4,11 +4,11 @@ class PurchaseShipping
                 :city, :address, :building, :phone, :purchase_id, :token
 
     with_options presence: true do
-    validates :user_id, :item_id, :address, :token
+    validates :user_id, :item_id, :city, :address, :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :phone, format: {with: /\A\d{10,11}\z/}
-    end
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    end
 
 
   def save
--- spec/models/purchase_shipping_spec.rb
+++ spec/models/purchase_shipping_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseShipping, type: :model do
-  
+  before do
+    item = FactoryBot.create(:item)
+    user = FactoryBot.create(:user)
+    @purchase_shipping = FactoryBot.build(:purchase_shipping, item_id: item.id, user_id: user.id)
+  end
+
+  describe '商品購入' do
+    context '商品を購入できる' do
+      it '全ての項目が正しく入力されている' do
+        expect(@purchase_shipping).to be_valid
+      end
+      it 'buildingが空でも購入できる' do
+        @purchase_shipping.building = ""
+        expect(@purchase_shipping).to be_valid
+      end
+    end
+
+    context '商品を購入できない' do
+      it 'tokenが生成されていない' do
+        @purchase_shipping.token = ""
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'postal_codeが空' do
+        @purchase_shipping.postal_code = ""
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'cityが空' do
+        @purchase_shipping.city = ""
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("City can't be blank")
+      end
+      it 'addressが空' do
+        @purchase_shipping.address = ""
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'phoneが空' do
+        @purchase_shipping.phone = ""
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Phone can't be blank")
+      end
+      it 'postal_codeが全角数字' do
+        @purchase_shipping.postal_code = "１２３-４５６７"
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Postal code is invalid")
+      end
+      it 'postal_codeにハイフンがない' do
+        @purchase_shipping.postal_code = '1234567'
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Postal code is invalid")
+      end
+      it 'phoneに数字以外が含まれる' do
+        @purchase_shipping.phone = "010-1234-5678"
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Phone is invalid")
+      end
+      it 'phoneがが9字以下' do
+        @purchase_shipping.phone = "123456789"
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Phone is invalid")
+      end
+      it 'phoneがが12字以上' do
+        @purchase_shipping.phone = "000123456789"
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Phone is invalid")
+      end
+      it 'userが紐づいていない' do
+        @purchase_shipping.user_id = nil
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐づいていない' do
+        @purchase_shipping.item_id = nil
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
 end
